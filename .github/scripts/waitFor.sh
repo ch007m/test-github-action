1942b8c9f4489c90d5076830376be414f89ad56d
cmoulliard <cmoulliard@redhat.com>
2024-06-14 10:26:47 +0200

    Improving the script
    Signed-off-by: cmoulliard <cmoulliard@redhat.com>

--- .github/scripts/waitFor.sh
+++ .github/scripts/waitFor.sh
@@ -4,26 +4,22 @@
 # ./waitFor.sh pod operators Running "name -o jsonpath='{.status.phase}'"
 
 RESOURCE="${1}"
-NAMESPACE="${2}"
-EXPECTED="${3}"
-EXTRA="${4-}"
+NAME="${2}"
+NAMESPACE="${3}"
+EXPECTED="${4}"
+EXTRA="${5-}"
 
 retries=30
 until [[ $retries == 0 ]]; do
-  actual=$(kubectl get $RESOURCE -n $NAMESPACE $EXTRA 2>/dev/null || echo "Waiting for $RESOURCE -> $EXPECTED to appear")
+  actual=$(kubectl get $RESOURCE $NAME -n $NAMESPACE $EXTRA 2>/dev/null || echo "Waiting for $RESOURCE/$NAME -> $EXPECTED to appear")
   if [[ "$actual" =~ .*"$EXPECTED".* ]]; then
-    echo "Resource \"$RESOURCE\" found with: $actual" 2>&1
+    echo "Resource \"$RESOURCE/$NAME\" found" 2>&1
+    echo "$actual" 2>&1
     break
   else
-    echo "Waiting for resource \"$RESOURCE\" actual: $actual" 2>&1
+    echo "Waiting for resource \"$RESOURCE/$NAME\" ..." 2>&1
+    echo "$actual" 2>&1
   fi
   sleep 10s
   retries=$((retries - 1))
 done
-
-if [[ $retries == 0 ]]; then
-  echo "Failed to get $RESOURCE" 2>&1
-  exit 1
-else
-  exit 0
-fi
